5244f2f4e735c2508b1badc2ee4e043a640eb2d5
AkaTordu <maximilienlandes@gmail.com>
2024-05-17 11:26:32 +0200

    init openApi

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Product Management API')
-    .setDescription('API for managing products')
+    .setTitle('Gestion Produits API')
+    .setDescription('API pour la gestion des produits')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
