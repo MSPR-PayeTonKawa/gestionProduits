0693353f0f6b38f3eef237d8df78e24dc5c22843
Germain-L <germain.leignel@gmail.com>
2024-06-05 14:29:15 +0200

    chore: Update npm dependencies and delete tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsModule } from './products/products.module';
 import { Product } from './products/entities/product.entity';
+import 'dotenv/config'
+
+console.log(process.env.DATABASE_HOST)
+console.log(process.env.DATABASE_PORT)
+console.log(process.env.POSTGRES_USER)
+console.log(process.env.POSTGRES_PASSWORD)
+console.log(process.env.POSTGRES_DB)
 
 @Module({
   imports: [
