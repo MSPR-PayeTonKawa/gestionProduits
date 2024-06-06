6026734b9594c94905ab32c6bd7c4432912d6c16
AkaTordu <maximilienlandes@gmail.com>
2024-06-06 14:19:59 +0200

    fix swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { serve, setup } from 'swagger-ui-express';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,7 +14,13 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+
+  // Serve swagger using swagger-ui-express
+  const swaggerHtml = readFileSync(join(__dirname, '..', 'node_modules', 'swagger-ui-dist', 'index.html'), 'utf8')
+    .replace('https://petstore.swagger.io/v2/swagger.json', '/swagger-docs-json');
+
+  app.use('/api', serve, (req, res) => res.send(swaggerHtml));
+  app.use('/swagger-docs-json', (req, res) => res.json(document));
 
   await app.listen(3000);
 }
